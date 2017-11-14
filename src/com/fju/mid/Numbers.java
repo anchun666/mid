6bd82966c290a50324999a370162228136f7d077
Alvin Cheng <pop199890@gmail.com>
2017-11-14 14:12:21 +0800

    Numbers

---
+++ src/com/fju/mid/Numbers.java
@@ -0,0 +1,15 @@
+package com.fju.mid;
+import java.util.Scanner;
+public class Numbers {
+
+	public static void main(String[] args) {
+		System.out.println("請使用者輸入一個數字:");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		int number = Integer.parseInt(line);
+		System.out.println("1");
+		
+		
+
+	}
+}
